0ad68ce0ea3131483f02fc55e1a61408f92fc50e
Trevor Adams <hey@trevoradams.com>
2022-04-10 14:46:56 -0600

    task: connect to graphQL movies Query

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,33 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Movie from '../components/Movie'
 import Navbar from '../components/Navbar'
-import { movies } from '../data/movies' //TODO: Remove Me
+//import { movies } from '../data/movies' //TODO: Remove Fake Data
+
+import gql from "graphql-tag"
+import { useQuery } from "@apollo/client"
+
+const MoviesQuery = gql`
+  query MoviesQuery {
+  movies {
+    id
+    title
+    overview
+    imageUrl
+  }
+}
+`
 
 const Home: NextPage = () => {
+  const { loading, error, data } = useQuery(MoviesQuery, {
+    fetchPolicy: "cache-and-network",
+  })
+
+  if (loading) {
+    return <div>Loading ...</div>
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -19,7 +43,7 @@ const Home: NextPage = () => {
             role="list"
             className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
           >
-            {movies.map((movie) => (
+            {data.movies.map((movie) => (
               <li key={movie.id} className="relative">
                 <Movie movie={movie} />
               </li>
